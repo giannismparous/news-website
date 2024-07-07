68c55534aad2a7da91820a44fec7e33e29e2dab2
Giannis Mparous <p3190131@aueb.gr>
2024-07-08 01:59:30 +0300

    podcasts

--- src/pages/Podcasts.js
+++ src/pages/Podcasts.js
@@ -1,34 +1,26 @@
-// components/Podcasts.js
-
 import React from 'react';
 import '../styles/Podcasts.css';
 
 const Podcasts = () => {
-  // List of YouTube video URLs
-  const videoUrls = [
-    'https://www.youtube.com/embed/urOtaJpfl-I?si=8bUvKQimcN9lSyX2',
-    'https://www.youtube.com/embed/IyqDueEf7vM?si=1uK3GsQ2n_xOcfEG',
-    'https://www.youtube.com/embed/Jr2hgkey6X0?si=1ARBZk4NWfgWRLlL',
-    // Add more video URLs as needed
+  // List of Spotify podcast URLs
+  const podcastUrls = [
+    'https://open.spotify.com/embed/episode/0YrftJbot66wj3TzklkPBF/video?utm_source=generator',
+    'https://open.spotify.com/show/your-podcast-id-2',
+    // Add more podcast URLs as needed
   ];
 
   return (
     <div className="podcasts-container">
       <h2 className="podcasts-header">Δείτε τα τελευταία μας podcasts</h2>
-      {videoUrls.map((url) => (
-        <div className="video-container" key={url}>
-          <iframe
-            width="560"
-            height="315"
-            src={url}
-            title={`YouTube Video ${url}`}
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-      ))}
+      <iframe  src="https://open.spotify.com/embed/episode/0YrftJbot66wj3TzklkPBF/video?utm_source=generator" width="624" height="351" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
   );
 };
+
+// Function to extract Spotify podcast ID from URL
+function getPodcastId(url) {
+  const parts = url.split('/');
+  return parts[parts.length - 1]; // Assuming the last part of URL is the podcast ID
+}
 
 export default Podcasts;
